d5cefa0dc238ca00d3d1e307ef928ffbddbd64b4
PriestOfFerns <timofeydenin@gmail.com>
2022-11-15 17:05:02 +0300

    Added The OOP Stuff
    did a thing

---
+++ OOP-Test.py
@@ -0,0 +1,25 @@
+
+
+class Car: # Класс Машина
+    def __init__(self,  speed, wheels, size, brand): # инициализация
+        self.wheels = wheels # Количество колес
+        self.speed = speed   # скорость
+        self.size = size # Размер
+        self.brand = brand
+    def changeSpeed(self, Newspeed): # Метод изменить скорость
+        self.speed = Newspeed
+        print("vroom vroom")
+
+class SUV(Car): # Класс Внедорожник
+
+    def __init__(self, speed, brand):  # инициализация
+        super().__init__(speed,4,"medium",brand)
+    def driveOffroad(self): #метод "езда по бездорожью"
+        print("Am now driving offroad ")
+
+
+class Ford(SUV):
+    def __init__(self, speed):  # инициализация
+        super().__init__(speed,"Ford")
+
+FordF150 = Ford(0)
